02e014af0f1a812b04efc1e7efc9184f12d5d897
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-14 14:24:43 -0400

    test

--- app/main.js
+++ app/main.js
@@ -3,7 +3,10 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
-    console.log(connection);
+    connection.on('data',data=>{
+        console.log(data);
+        connection.write(`+PONG\r\n`)
+    })
 })
 server.listen(6379, "127.0.0.1");
 // Uncomment this block to pass the first stage
