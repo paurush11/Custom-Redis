117244e533ad9d5655c94749d7e09b5b4d188e81
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-14 14:44:09 -0400

    test

--- app/main.js
+++ app/main.js
@@ -4,16 +4,9 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
     connection.on('data', data => {
-
-        console.log(JSON.parse(data));
+        console.log(data.toString())
         console.log("jhi");
         connection.write(`+PONG\r\n`)
     })
 })
 server.listen(6379, "127.0.0.1");
-// Uncomment this block to pass the first stage
-// const server = net.createServer((connection) => {
-//   // Handle connection
-// });
-//
-// server.listen(6379, "127.0.0.1");
