0501e74607ad63d0aeb60f942cc8f5ec7d342320
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-14 14:21:18 -0400

    test

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
     console.log(connection);
 })
-server.listen(4001, "127.0.0.1");
+server.listen(6379, "127.0.0.1");
 // Uncomment this block to pass the first stage
 // const server = net.createServer((connection) => {
 //   // Handle connection
