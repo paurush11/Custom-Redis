5f7004cd146e39af084a44aed858069533ca1813
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-17 13:27:38 -0400

    test

--- app/main.js
+++ app/main.js
@@ -96,6 +96,9 @@ const server = net.createServer((connection) => {
         master_replid: "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb",
         master_repl_offset: 0
     }
+    if (role === "slave") {
+        connection.write(`*1\r\n$4\r\nping\r\n`)
+    }
 
     if (!clientParsers.has(clientId)) {
         clientParsers.set(clientId, new Parser(port, info));
