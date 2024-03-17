b4f5eede308210b0484b8a745d79ab05e4922d75
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-17 14:48:55 -0400

    test

--- app/main.js
+++ app/main.js
@@ -67,7 +67,7 @@ const handleParserCommands = (data, parser, connection) => {
 
 const handleHandshake = (parser, connection) => {
     const role = masterSlavePorts.has(port) ? 'slave' : 'master';
-    const masterHost = masterSlavePorts.get(port).split(":")
+    const masterHost = masterSlavePorts.get(port)
     console.log(masterHost)
     // const masterSlaveConnection = net.createConnection({ host: args[masterHostIndex], port: args[masterPortIndex] }, () => {
     //     masterSlaveConnection.write(`*1\r\n$4\r\nping\r\n`)
