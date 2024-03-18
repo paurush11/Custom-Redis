c162178b84f2b4347ad9299cca00710ce245b9b4
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-18 09:06:50 -0400

    test

--- app/parser.js
+++ app/parser.js
@@ -59,6 +59,7 @@ class Parser {
 
     parseInput() {
         if (this.data[0] === "*") {
+            console.log(data)
             const values = this.data.slice(1).split("\r\n").filter((val, index) => !(index & 1));
             const length = values[0];
             console.log(values);
