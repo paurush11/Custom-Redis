4da0672b4ed5695f2dcd472442db60105497fd91
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-18 10:37:41 -0400

    test

--- app/main.js
+++ app/main.js
@@ -41,10 +41,7 @@ const sendRDBFile = (connection) => {
 const sendReplicaCommands = (parser, data) => {
     if (parser.INFO.role !== 'master' || replicaList.length == 0) return;
     for (const [replica, replicaPort] of replicaList) {
-        // const val = [data.toString()];
-        // console.log(val);
         replica.write(data);
-        // console.log(parser.savedDict)
         // Iterate through all the key, val in masterSlavePorts to see if val is host:port of current parsers.
         const replicaClientId = createClientId(replica);
 
@@ -55,7 +52,7 @@ const sendReplicaCommands = (parser, data) => {
         replicaParser.savedDict = parser.savedDict
 
         handleGetCommand(replicaParser, replica);
-        // replicaParser.setData(data.toString());
+        replicaParser.setData(data.toString());
     }
 }
 const handlePSYNCCommand = (parser, connection) => {
