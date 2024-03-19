7ba3a70dfc18e3528eae7fc9bb725f8d878daf0b
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-19 15:23:26 -0400

    test

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ const clientParsers = new Map();
 const masterSlavePorts = new Map();
 const emptyRDBFileHex = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
 const replicaList = [];
-
+let handShakeCount = 0;
 
 
 const handlePing = (parser, connection) => {
@@ -122,10 +122,8 @@ const handleHandshake = () => {
                 if (replicaParser.mappedValues["REPLCONF"]) {
                     slaveSlaveConnection.write(replicaParser.mappedValues["REPLCONF"]);
                 }
-
-
+                handShakeCount += 1;
             })
-
         })
 
     }
@@ -176,10 +174,12 @@ const server = net.createServer((connection) => {
 
     connection.on('data', data => {
         handleParserCommands(data, parser, connection);
+        console.log(handShakeCount)
     })
     connection.on('close', () => {
         clientParsers.delete(clientId);
     });
+
 
 })
 
