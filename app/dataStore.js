3f5311c40fa3722c4c0c65e57738a72bba0d1fc4
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-21 12:54:15 -0400

    test

--- app/dataStore.js
+++ app/dataStore.js
@@ -30,23 +30,75 @@ class dataStore {
         return "stream"
     }
 
-    insertStream(key, value, stream_key) {
-        const [millisecondsTime, sequenceNumber] = stream_key.split("-");
-        if (this.streamCursor === 0) {
-            if (millisecondsTime === '0' && sequenceNumber === '0')
-                return Encoder.generateStreamError(false);
-        } else {
-            const [prevMillisecondsTime, prevSequenceNumber] = this.streamTimeStamps[this.streamCursor - 1].split("-");
-            if (millisecondsTime === '0' && sequenceNumber === '0')
-                return Encoder.generateStreamError(false);
-            if (millisecondsTime < prevMillisecondsTime || (prevMillisecondsTime === millisecondsTime && sequenceNumber <= prevSequenceNumber)) {
-                return Encoder.generateStreamError(true);
-            }
+    compareStreamValues(ms, sq, pms, psq) {
+        if (ms === '0' && sq === '0')
+            return Encoder.generateStreamError(false);
+        if (sq === "*") {
 
         }
-        this.streamTimeStamps.push(stream_key);
-        this.streamCursor += 1;
-        this.map.set(key, value);
+    }
+
+    insertStream(key, value, stream_key) {
+
+        if (stream_key === "*") {
+            if (this.streamCursor === 0) {
+                let newSequenceNumber = '0'
+                let newMillisecondsTime = '1526919030474'
+                this.streamTimeStamps.push([newMillisecondsTime, newSequenceNumber].join("-"));
+                this.streamCursor += 1;
+                this.map.set(key, value);
+            } else {
+                const [prevMillisecondsTime, prevSequenceNumber] = this.streamTimeStamps[this.streamCursor - 1].split("-");
+                let newSequenceNumber = '0'
+                let newMillisecondsTime = '1526919030474'
+            }
+        } else {
+            const [millisecondsTime, sequenceNumber] = stream_key.split("-");
+
+            if (sequenceNumber === '*') {
+                if (this.streamCursor === 0) {
+                    let newSequenceNumber = millisecondsTime === '0' ? '1' : '0'
+                    let newMillisecondsTime = millisecondsTime
+                    this.streamTimeStamps.push([newMillisecondsTime, newSequenceNumber].join("-"));
+                    this.streamCursor += 1;
+                    this.map.set(key, value);
+
+                } else {
+                    const [prevMillisecondsTime, prevSequenceNumber] = this.streamTimeStamps[this.streamCursor - 1].split("-");
+                    if (millisecondsTime < prevMillisecondsTime) {
+                        return Encoder.generateStreamError(true);
+                    } else if (prevMillisecondsTime === millisecondsTime) {
+                        let newSequenceNumber = (Number(prevSequenceNumber) + 1).toString();
+                        let newMillisecondsTime = millisecondsTime
+                    } else {
+                        let newSequenceNumber = '0'
+                        let newMillisecondsTime = millisecondsTime
+                    }
+
+                    this.streamTimeStamps.push([newMillisecondsTime, newSequenceNumber].join("-"));
+                    this.streamCursor += 1;
+                    this.map.set(key, value);
+                }
+
+            } else {
+                let newSequenceNumber = sequenceNumber
+                let newMillisecondsTime = millisecondsTime
+                if (this.streamCursor === 0) {
+                    if (millisecondsTime === '0' && sequenceNumber === '0')
+                        return Encoder.generateStreamError(false);
+                } else {
+                    const [prevMillisecondsTime, prevSequenceNumber] = this.streamTimeStamps[this.streamCursor - 1].split("-");
+                    if (millisecondsTime === '0' && sequenceNumber === '0')
+                        return Encoder.generateStreamError(false);
+                    if (millisecondsTime < prevMillisecondsTime || (prevMillisecondsTime === millisecondsTime && sequenceNumber <= prevSequenceNumber)) {
+                        return Encoder.generateStreamError(true);
+                    }
+                }
+                this.streamTimeStamps.push([newMillisecondsTime, newSequenceNumber].join("-"));
+                this.streamCursor += 1;
+                this.map.set(key, value);
+            }
+        }
     }
 
     has() {
