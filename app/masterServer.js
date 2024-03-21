381767015e605664e99b5a2a0d34b6cb19549e1e
Paurush Batish <paurushbatishfbd@gmail.com>
2024-03-21 15:39:27 -0400

    test

--- app/masterServer.js
+++ app/masterServer.js
@@ -106,7 +106,8 @@ class MasterServer {
         const stream_key = args[1];
         const stream_key_start_value = args[2];
         const stream_key_end_value = args[3];
-        return this.dataStore.getStreamValues(stream_key, stream_key_start_value, stream_key_end_value);
+        const value = this.dataStore.getStreamValues(stream_key, stream_key_start_value, stream_key_end_value);
+        return value
     }
 
     handleStreams(args) {
